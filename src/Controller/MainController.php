bd2dcd7841c762c6a68611e94b9369d417d93569
Elvisthegreat <amadinelvis6@gmail.com>
2025-04-26 19:45:39 +0200

    added first html twig template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AbstractAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starshipt parts!');
+        $myShip = [
+            "name" => "Star Voyager",
+            "type" => "Cargo",
+            "capacity" => 5000,
+            "destination" => "Mars",
+        ];
+
+        $starshipCount = 457;
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
